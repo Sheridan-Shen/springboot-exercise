4561e10644cadc2a0c5c6ddfc133699ff0b553c2
Sheridan <shensh5@oocl.com>
2025-08-27 14:09:08 +0800

    [Company API]feat:添加CompanyRepository

--- src/main/java/com/oocl/springboot_exercise/service/CompanyService.java
+++ src/main/java/com/oocl/springboot_exercise/service/CompanyService.java
@@ -2,6 +2,7 @@ package com.oocl.springboot_exercise.service;
 
 import com.oocl.springboot_exercise.models.Company;
 import com.oocl.springboot_exercise.models.Employee;
+import com.oocl.springboot_exercise.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -13,33 +14,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class CompanyService {
-//    private Map<Integer, Company> companyDb = new HashMap<>();
-    private final HashMap<Integer, Company> companyDb = new HashMap<>(Map.of(
-            1, new Company(1, "Acme Corporation", List.of(
-                    new Employee(1, "John Smith", 32, "MALE", 5000.0),
-                    new Employee(2, "Jane Johnson", 28, "FEMALE", 6000.0)
-            )),
-            2, new Company(2, "TechCom Solutions", List.of(
-                    new Employee(3, "David Williams", 35, "MALE", 5500.0),
-                    new Employee(4, "Emily Brown", 23, "FEMALE", 4500.0),
-                    new Employee(5, "Michael Jones", 40, "MALE", 7000.0)
-            )),
-            3, new Company(3, "Global Innovators"),
-            4, new Company(4, "Stellar Enterprises"),
-            5, new Company(5, "Nexus Industries")
-    ));
-    private AtomicInteger companyIdGenerator = new AtomicInteger(6);
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
 
     public List<Company> getAllCompanies() {
-        return new ArrayList<>(companyDb.values());
+        return new ArrayList<>(companyRepository.getCompanies().values());
     }
 
     public Company getCompanyById(Integer id) {
-        return companyDb.get(id);
+        return companyRepository.getCompany(id);
     }
 
     public List<Employee> getEmployeesByCompanyId(Integer companyId) {
-        Company company = companyDb.get(companyId);
+        Company company = companyRepository.getCompany(companyId);
         if (company != null) {
             return company.getEmployees();
         }
@@ -47,14 +37,12 @@ public class CompanyService {
     }
 
     public Company addCompany(Company company) {
-        Integer newId = companyIdGenerator.incrementAndGet();
-        company.setId(newId);
-        companyDb.put(newId, company);
+        companyRepository.addCompany(company);
         return company;
     }
 
     public Company updateCompany(Integer id, Company companyDetails) {
-        Company company = companyDb.get(id);
+        Company company = companyRepository.getCompany(id);
         if (company != null) {
             company.setName(companyDetails.getName());
             return company;
@@ -63,6 +51,6 @@ public class CompanyService {
     }
 
     public boolean deleteCompany(Integer id) {
-        return companyDb.remove(id) != null;
+        return companyRepository.deleteCompany(id);
     }
 }
